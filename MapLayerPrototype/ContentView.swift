146188c83f56189d61db2952ad1aa24298a08645
杉岡成哉 <a1209s518n928@gmail.com>
2025-10-18 20:45:44 +0900

    Initial Commit

---
+++ MapLayerPrototype/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapLayerPrototype
+//
+//  Created by 杉岡成哉 on 2025/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapLayerPrototype/MapLayerPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapLayerPrototypeApp.swift
+//  MapLayerPrototype
+//
+//  Created by 杉岡成哉 on 2025/10/18.
+//
+
+import SwiftUI
+
+@main
+struct MapLayerPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
